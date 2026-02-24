a84274a05bfa73f8304b4f4940d957974bab5807
estherbirngruber <estherbirngruber@gmail.com>
2026-02-24 20:40:13 +0100

    edited struct and functions

--- include/statistics.h
+++ include/statistics.h
@@ -16,6 +16,13 @@ typedef struct {
     int cars_in [SIMULATION_TIME]
     int cars_out [SIMULATION_TIME]
     int length_queue [SIMULATION_TIME]
-    int last_waiting_time [SIMULATION_TIME]
+    int last_wait_time [SIMULATION_TIME]
+    int max_wait_time
+    int avg_parkhaus_auslastung
+    int avg_length_queue
+    int max_length_queue
+    int avg_wait_time
+    int all_cars_in
+    int all_cars_out
 }stats
 */
--- src/statistics.c
+++ src/statistics.c
@@ -5,19 +5,45 @@
 
 // initstats (arrat für die ganzen Variablen die gemittelt werden soll) Große: Anzahl der Simulationsschrtte
 
-FUNCTION updateStats (current_time, parked_cars, cars_in, cars_out, length_queue, last_waiting_time):
+FUNCTION updateStats (current_time, parked_cars, cars_in, cars_out, length_queue, last_waiting_time){
     stats.parkhaus_auslastung[current_time] = parked_cars
     stats.cars_in[current_time] = cars_in
     stats.cars_out[current_time] = cars_out
     stats.length_queue [current_time] = length_queue
-    stats.last_waiting_time [current_time] = last_waiting_time
+    stats.last_wait_time [current_time] = last_wait_time
 
+    IF last_wait_time > stats.max_wait_time{
+        stats.max_wait_time = last_wait_time
+    }
+    IF length_queue > stats.max_length_queue {
+        stats.max_length_queue = length_queue
+    }
+    stats.all_cars_in += cars_in_step
+    stats.all_cars_out += cars_out_step
     
+}
+
+FUNCTION printRuntimeStats (current_time, parked_cars, cars_in, cars_out, length_queue, last_waiting_time){
+    PRINT {
+            "Zeit seit Sim. Beginn:"    current_time
+            "Parkhausauslastung:"       parked_cars "von" ANZAHL_STELLPLAETZE "Plaetze belegt"
+            "Autos rein/raus:"          cars_in "/" cars_out
+            "Laenge Warteschlange:"     length_queue "Autos + " cars_in "neue Autos"
+            "Letzte Wartezeit:"         last_waiting_time
+    }
+
+}
 
 
-FUNCTION printRuntimeStats ()
+FUNCTION printFinalStats ()
+    PRINT {
+            "Simulationsdauer:"                 SIMULATION_TIME
+            "Auslastung Parkhaus Ø:"            
+            "Länge Warteschlange Ø:"           
+            "Max. Länge Warteschlange:"         
+            "Wartezeit Ø:"                      
+            "ges. Anzahl Fahrzeuge rein/raus:"  
+    }
 
-
-FUNCTION printFinalStats
 
 FUNCTION writeStatsToFile
