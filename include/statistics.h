20439f6f1e645529f68f457a8c78aff99dbc6137
estherbirngruber <estherbirngruber@gmail.com>
2026-02-25 09:31:23 +0100

    removed arrays and finished all funtions

--- include/statistics.h
+++ include/statistics.h
@@ -1,5 +1,4 @@
 /*
-initstats (arrat für die ganzen Variablen die gemittelt werden soll) Große: Anzahl der Simulationsschrtte
 
 printRuntimeStats
 
@@ -9,19 +8,12 @@ updateStats
 
 writeStatsToFile
 
-updateLängstewartezeit
-
 typedef struct {
-    int parkhaus_auslastung [SIMULATION_TIME]
-    int cars_in [SIMULATION_TIME]
-    int cars_out [SIMULATION_TIME]
-    int length_queue [SIMULATION_TIME]
-    int last_wait_time [SIMULATION_TIME]
     int max_wait_time
-    int avg_parkhaus_auslastung
-    int avg_length_queue
+    int sum_parkhaus_auslastung
+    int sum_length_queue
     int max_length_queue
-    int avg_wait_time
+    int _wait_time
     int all_cars_in
     int all_cars_out
 }stats
--- src/statistics.c
+++ src/statistics.c
@@ -5,45 +5,49 @@
 
 // initstats (arrat für die ganzen Variablen die gemittelt werden soll) Große: Anzahl der Simulationsschrtte
 
-FUNCTION updateStats (current_time, parked_cars, cars_in, cars_out, length_queue, last_waiting_time){
-    stats.parkhaus_auslastung[current_time] = parked_cars
-    stats.cars_in[current_time] = cars_in
-    stats.cars_out[current_time] = cars_out
-    stats.length_queue [current_time] = length_queue
-    stats.last_wait_time [current_time] = last_wait_time
-
-    IF last_wait_time > stats.max_wait_time{
-        stats.max_wait_time = last_wait_time
+FUNCTION updateStats (pstats : pointer to stats, parked_cars, cars_in, cars_out, length_queue, last_wait_time){
+    IF last_wait_time > pstats -> max_wait_time{
+        pstats -> max_wait_time = last_wait_time
     }
-    IF length_queue > stats.max_length_queue {
-        stats.max_length_queue = length_queue
+    IF length_queue > pstats -> max_length_queue {
+        pstats -> max_length_queue = length_queue
     }
-    stats.all_cars_in += cars_in_step
-    stats.all_cars_out += cars_out_step
+    pstats -> all_cars_in += cars_in
+    pstats -> all_cars_out += cars_out
     
+    pstats -> sum_parkhaus_auslastung += parked_cars
+    pstats -> sum_length_queue += length_queue
+    pstats -> sum_wait_time += last_wait_time
 }
 
-FUNCTION printRuntimeStats (current_time, parked_cars, cars_in, cars_out, length_queue, last_waiting_time){
-    PRINT {
+FUNCTION printRuntimeStats (current_time, parked_cars, cars_in, cars_out, length_queue, car_in_queue, last_wait_time){
+    PRINT:
             "Zeit seit Sim. Beginn:"    current_time
             "Parkhausauslastung:"       parked_cars "von" ANZAHL_STELLPLAETZE "Plaetze belegt"
             "Autos rein/raus:"          cars_in "/" cars_out
-            "Laenge Warteschlange:"     length_queue "Autos + " cars_in "neue Autos"
-            "Letzte Wartezeit:"         last_waiting_time
-    }
-
+            "Laenge Warteschlange:"     length_queue "Autos + " car_in_queue "neue Autos"
+            "Letzte Wartezeit:"         last_wait_time
 }
 
 
-FUNCTION printFinalStats ()
-    PRINT {
+FUNCTION printFinalStats (stats){
+    PRINT:
             "Simulationsdauer:"                 SIMULATION_TIME
-            "Auslastung Parkhaus Ø:"            
-            "Länge Warteschlange Ø:"           
-            "Max. Länge Warteschlange:"         
-            "Wartezeit Ø:"                      
-            "ges. Anzahl Fahrzeuge rein/raus:"  
-    }
+            "Auslastung Parkhaus Ø:"            stats.sum_parkhaus_auslastung/SIMULATION_TIME * 100
+            "Länge Warteschlange Ø:"            stats.sum_length_queue/SIMULATION_TIME * 100
+            "Max. Länge Warteschlange:"         stats.max_length_queue
+            "Wartezeit Ø:"                      stats.sum_wait_time/SIMULATION_TIME * 100
+            "ges. Anzahl Fahrzeuge rein/raus:"  stats.all_cars_in " / " stats.all_cars_out
+}
 
-
-FUNCTION writeStatsToFile
+FUNCTION writeStatsToFile (stats){
+    CREATE new file "statistics.txt" with random number
+    WRITE final stats to file:
+            "Simulationsdauer:"                 SIMULATION_TIME
+            "Auslastung Parkhaus Ø:"            stats.sum_parkhaus_auslastung/SIMULATION_TIME * 100
+            "Länge Warteschlange Ø:"            stats.sum_length_queue/SIMULATION_TIME * 100
+            "Max. Länge Warteschlange:"         stats.max_length_queue
+            "Wartezeit Ø:"                      stats.sum_wait_time/SIMULATION_TIME * 100
+            "ges. Anzahl Fahrzeuge rein/raus:"  stats.all_cars_in " / " stats.all_cars_out
+    CLOSE file
+}
