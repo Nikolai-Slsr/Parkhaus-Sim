df95d1089294ae23e943c554c8bd5e5d165a765e
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-02-28 07:27:06 +0100

    fix vehicle.h to use typedef

--- include/vehicle.h
+++ include/vehicle.h
@@ -7,13 +7,13 @@
  * Stores all time and identification data needed for the queue, entry and parking duration.
  * 
  */
-struct vehicle
+typedef struct vehicle
 {
     int vehicle_id; //unique identifier for the vehicle
     int remaining_parktime; //this is specified by the instructions but actually it's redundant information because we can calculate this from
     int time_of_entry; //time of entry into the parking lot is used to calculate the remaining parking time
     int time_of_arrival; // Us used to calculate the waiting time in the queue
     int random_park_duration; // between 1 and max_parking_time
-};
+}vehicle;
 
 #endif
