29c1543e30706d0fde11c9cf3537b010efac54c1
estherbirngruber <estherbirngruber@gmail.com>
2026-03-10 21:45:34 +0100

    check if time step is zero

--- src/statistics.c
+++ src/statistics.c
@@ -206,8 +206,13 @@ void printFinalStats (const stats *pstats, const sim_parameters *pSim_parameters
     printf ("|");
 
     printf("\n\n%-35s %-40u", "Simulationsdauer:", pSim_parameters->time_steps);
-    printf("\n%-35s %-40u%c", "Auslastung Parkhaus Ø:", pstats->sum_parkhaus_auslastung/pSim_parameters->time_steps, '%');
-    printf("\n%-35s %-40u Autos", "Länge Warteschlange Ø:", pstats->sum_length_queue/pSim_parameters->time_steps);
+    if (pSim_parameters->time_steps != 0){
+        printf("\n%-35s %-40u%c", "Auslastung Parkhaus Ø:", pstats->sum_parkhaus_auslastung/pSim_parameters->time_steps, '%');
+        printf("\n%-35s %-40u Autos", "Länge Warteschlange Ø:", pstats->sum_length_queue/pSim_parameters->time_steps);
+    }else {
+        printf("Auslastung Parkhaus Ø: 0");
+        printf("Länge Warteschlange Ø: 0 Autos");
+    }
     printf("\n%-35s %-40u Autos", "Max. Laenge Warteschlange:", pstats->max_length_queue);
     double avg_wait_time;
     if (pstats->sum_cars_in > 0) {
@@ -249,8 +254,10 @@ void writeFinalStatsToFile (const stats *pstats, const sim_parameters *pSim_para
     fprintf (final_stats,"|");
 
     fprintf(final_stats,"\n\n%-35s %-40u", "Simulationsdauer:", pSim_parameters->time_steps);
+    if (pSim_parameters->time_steps != 0){
     fprintf(final_stats,"\n%-35s %-40u%c", "Auslastung Parkhaus Ø:", pstats->sum_parkhaus_auslastung/pSim_parameters->time_steps, '%');
-    fprintf(final_stats,"\n%-35s %-40u Autos", "Länge Warteschlange Ø:", pstats->sum_length_queue/pSim_parameters->time_steps);
+    fprintf(final_stats,"\n%-35s %-40u Autos", "Länge Warteschlange Ø:", pstats->sum_length_queue/pSim_parameters->time_steps);   
+    }
     fprintf(final_stats,"\n%-35s %-40u Autos", "Max. Laenge Warteschlange:", pstats->max_length_queue);
     unsigned int avg_wait_time;
     if (pstats->sum_cars_in > 0) {
