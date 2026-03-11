c2ec0e44f08693e01f2b87c1adb831f41d89b263
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-03-11 18:43:46 +0100

    moved pseudocode from the .c files to seperate files

--- src/queue.c
+++ src/queue.c
@@ -1,26 +1,9 @@
-/*
-#include "../include/queue.h"
-#include <stdlib.h>
-#include "../include/vehicle.h"
-
-node and queue structs are defined in include/queue.h, so we can use them here without redefining them
-vehicle struct is defined in include/vehicle.h, so we can use it here also
-*/
 #include "../include/queue.h"
 #include <stdlib.h>
 #include "../include/vehicle.h"
 #include <stdio.h>
 #include <assert.h>
 
-// FUNCTION queue *init_queue()
-//     allocate memory for a new queue
-//     IF memory allocation fails THEN
-//         RETURN NULL to indicate failure
-//     END IF
-//     set firstCar and lastCar to NULL
-//     set size to 0
-//     RETURN the initialized queue pointer
-// END FUNCTION
 queue *init_queue(){
     queue *p_new_queue = calloc(1, sizeof(queue)); //use calloc to automatically initalize the queue with 0 and NULL
     if (p_new_queue == NULL)
@@ -30,43 +13,6 @@ queue *init_queue(){
     return p_new_queue;
 }
 
-/*
-FUNCTION int enqueue(queue *queue, int id, int parking_time, int current_time)
-    if the queue pointer is NULL THEN
-        RETURN -1 to indicate failure
-    END IF
-
-    allocate memory for a new vehicle
-    IF memory allocation for vehicle fails THEN
-        RETURN -1 to indicate failure
-    END IF
-    set vehicle_id to id
-    set time_of_entry to -1 -> to show it hasn't entered the parking lot yet
-    set remaining_parktime to parking_time
-    set time_of_arrival to current_time
-    set random_park_duration to parking_time
-
-    allocate memory for a node to store the vehicle
-
-    IF memory allocation for node fails THEN
-        Free the memory allocated for the vehicle to avoid memory leaks
-        RETURN -1 to indicate failure
-    END IF
-    set the node's vehicle pointer to the new vehicle
-    set the node's next pointer to NULL
-
-    IF the queue is empty THEN
-        set first_node and last_node to the new node
-        increment size
-        RETURN 0 to indicate success
-    ELSE
-        set lastCar's next pointer to the new node
-        update lastCar to the new node
-        increment size
-        RETURN 0 to indicate success
-    END IF
-END FUNCTION
-*/
 int enqueue(queue *p_queue, int id, int parking_time, int current_time){
     if (p_queue == NULL) //check for a valid queue
     {
@@ -115,28 +61,6 @@ int enqueue(queue *p_queue, int id, int parking_time, int current_time){
         return 0;
     }
 }
-/*
-FUNCTION vehicle *dequeue(queue *queue)
-    IF the queue->size is 0 OR queue ist NULL THEN
-        RETURN -1
-    END IF
-
-    IF queue->length is 1 THEN
-        save pointer to vehicle in the first node
-        free the memory of the node
-        decrement size
-        set firstCar and lastCar to NULL
-        RETURN pointer to saved vehicle
-    ELSE
-        save pointer to vehicle in the first node
-        save a pointer to the first node (the one to be removed)
-        update queue->firstCar to point to the second node in the queue
-        free the memory of the node
-        decrement size
-        RETURN pointer to saved vehicle
-    END IF
-END FUNCTION
-*/
 
 vehicle *dequeue(queue *p_queue){
     if (p_queue == NULL || p_queue->size == 0)  //check for a valid queue and if the queue is not empty
@@ -165,20 +89,7 @@ vehicle *dequeue(queue *p_queue){
         return p_removed_vehicle;
     }
 }
-/*
-FUNCTION int print_queue(queue *queue)
-    IF the queue is empty or NULL THEN
-        RETURN -1
-    ELSE
-        initialize a pointer to the first node
-        WHILE: the current_node is not NULL DO
-            print the vehicle's id and the positon in the queue
-            move the pointer to the next vehicle in the queue
-        END WHILE
-        RETURN 0
-    END IF
-END FUNCTION
-*/
+
 int print_queue(queue *p_queue){
     //check whether the queue is printable (there is no point in printing an empty queue, and we also have to check for a valid queue)
     if (p_queue == NULL || p_queue->size == 0)  //this check works because if the queue is NULL the second part of the condition will not be evaluated due to short-circuit evaluation, so we won't get a segmentation fault by trying to access the size of a NULL pointer
@@ -200,28 +111,7 @@ int print_queue(queue *p_queue){
         return 0;                               //indicates a successful print
     }
 }
-/*
-FUNCTION int free_queue(queue **queue)
-    IF the queue pointer is NULL THEN
-        RETURN -1 to indicate failure
-    END IF
-    IF queue->size = 0 THEN
-        free the memory of the queue itself
-        RETURN 0
-    ELSE
-        initialize a pointer to the first node
-        WHILE: the current_node is not NULL DO
-            save a tmp pointer to the next node
-            free the memory of the vehicle in the current node
-            free the memory of the current node
-            move to the next node using the tmp pointer
-        END WHILE
-        free the memory of the queue itself
-        set the original queue pointer to NULL //to avoid dangling pointers; this is why we use a double pointer other wise the pinter to the original queue would still point to the now freed memory, wich could cause a segmentation fault IF we try to access it after freeing the memory
-        RETURN 0
-    END IF
-END FUNCTION
-*/
+
 int free_queue(queue **p_queue){
     if (p_queue == NULL || *p_queue == NULL)            //check for a valid queue pointer and a valid queue
     {
