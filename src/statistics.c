870e224f3af0f612548235f0950039166f2db6fb
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-03-11 12:55:22 +0100

    fixed a few bus

--- src/statistics.c
+++ src/statistics.c
@@ -220,7 +220,7 @@ void printFinalStats (const stats *pstats, const sim_parameters *pSim_parameters
     } else {
         avg_wait_time = 0;
     }
-    printf("\n%-35s %-40u minuten", "Wartezeit Ø:", avg_wait_time);
+    printf("\n%-35s %-40f minuten", "Wartezeit Ø:", avg_wait_time);
     printf("\n%-35s +%-40u/-%u", "ges. Anzahl Fahrzeuge rein/raus:", pstats->sum_cars_in, pstats->sum_cars_out);
 
     printf ("\n|");
